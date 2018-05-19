0f15ab6fce2ddac61198dfd54f2bde7386f839f9
Noah Frederick <code@noahfrederick.com>
2018-05-19 12:17:38 -0400

    Add simple Flask server
    Setup roughly based on https://codeburst.io/full-stack-single-page-application-with-vue-js-and-flask-b1e036315532

---
+++ run.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__,
+            static_folder = './dist/static',
+            template_folder = './dist')
+
+@app.route('/api/message')
+def message():
+    response = {
+        'message': 'It works!'
+    }
+    return jsonify(response)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if app.debug:
+        # Forward request to NPM development server.
+        return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template('index.html')
